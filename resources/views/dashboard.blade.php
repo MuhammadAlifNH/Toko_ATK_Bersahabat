bcdafa01e8115784f6e3b7c243e18d31c4637343
MuhammadAlifNH <136277568+MuhammadAlifNH@users.noreply.github.com>
2024-07-05 05:45:03 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,21 +9,7 @@
         <h1 class="display-4">Selamat datang di Dashboard Toko ATK Bersahabat</h1>
     </div>
 
-    <div class="row">
-            @foreach($produks as $produk)
-                <div class="col-md-4">
-                    <div class="card mb-4 shadow-sm">
-                        <img src="{{ asset($produk->gambar_produk) }}" class="card-img-top" alt="Gambar Produk">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $produk->nama_produk }}</h5>
-                            <p class="card-text">Harga: Rp{{ number_format($produk->harga_produk, 0, ',', '.') }}</p>
-                            <a href="{{ route('produk.show', $produk->id) }}" class="btn btn-primary">Lihat Detail</a>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-
+    
     
 </body>
 </html>
